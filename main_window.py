910b2b432391b2fe3bd1f51b73155a0fd16b0968
vartanchik12 <147078735+vartanchik12@users.noreply.github.com>
2023-12-26 20:54:12 +0400

    Update main_window.py

--- main_window.py
+++ main_window.py
@@ -1,5 +1,5 @@
 import sys
-
+import os
 from PyQt5.QtCore import Qt, QEvent
 from PyQt5.QtGui import QIcon, QPixmap
 from PyQt5.QtWidgets import *
@@ -12,6 +12,15 @@ from iterator import Iterator
 
 class Window(QMainWindow):
     def __init__(self):
+        """
+            данная функция вызывает все необходимые методы для создания окна
+            parameters
+
+            self
+            returns
+
+            none
+        """
         super().__init__()
 
         self.initUI()
@@ -21,11 +30,24 @@ class Window(QMainWindow):
         self.createToolBar()
         self.dataset2_path = ''
         self.dataset3_path = ''
+        # self.annotation1_path = 'paths.csv'
+        # self.annotation2_path = ''
+        # self.annotation3_path = ''
+
+    def initUI(self):
+        """
+            данная функция создает главное окно и размещает кнопки по макету
+            parameters
+
+            self
+            returns
+
+            none
+        """
         self.annotation1_path = ''
         self.annotation2_path = ''
         self.annotation3_path = ''
 
-    def initUI(self):
         self.center()
         self.setWindowTitle('Roses and tulips')
         self.setWindowIcon(QIcon('img/main.png'))
@@ -55,13 +77,28 @@ class Window(QMainWindow):
         rose_btn.clicked.connect(self.nextRose)
         tulip_btn.clicked.connect(self.nextTulip)
 
-        self.folderpath = ' '
+        self.folderpath = 'dataset'
 
         self.showMaximized()
 
     def initIterators(self):
-        self.roses = Iterator('rose', 'dataset')
-        self.tulips = Iterator('tulip', 'dataset')
+        """
+            данная функция создает два объекта-итератора для показа изображений
+            parameters
+
+            self
+            returns
+
+            none
+        """
+        if 'dataset' in self.folderpath:
+            path_file_name = os.path.join(self.annotation1_path, 'paths.csv')
+        elif 'dataset2' in self.folderpath:
+            path_file_name = f'{self.annotation2_path}/paths2.csv'
+        else:
+            path_file_name = f'{self.annotation3_path}/paths3.csv'
+        self.roses = Iterator('rose', path_file_name)
+        self.tulips = Iterator('tulip', path_file_name)
 
     def nextRose(self):
         """
@@ -85,6 +122,15 @@ class Window(QMainWindow):
             self.nextRose()
 
     def nextTulip(self):
+        """
+            данная функция получает следующее изображение и размещает на главном окне
+            parameters
+
+            self
+            returns
+
+            none
+        """
         lbl_size = self.lbl.size()
         next_image = next(self.tulips)
         if next_image != None:
@@ -98,12 +144,31 @@ class Window(QMainWindow):
 
 
     def center(self):
+        """
+            центрирование главного окна относительно экрана
+
+            parameters
+
+            self
+            returns
+
+            none
+        """
         widget_rect = self.frameGeometry()
         pc_rect = QDesktopWidget().availableGeometry().center()
         widget_rect.moveCenter(pc_rect)
         self.move(widget_rect.center())
 
     def createMenuBar(self):
+        """
+            данная функция создает меню
+            parameters
+
+            self
+            returns
+
+            none
+        """
         menuBar = self.menuBar()
 
         self.fileMenu = menuBar.addMenu('&File')
@@ -117,6 +182,15 @@ class Window(QMainWindow):
         self.dataMenu.addAction(self.createData2Action)
 
     def createToolBar(self):
+        """
+            данная функция создает тулбар
+            parameters
+
+            self
+            returns
+
+            none
+        """
         fileToolBar = self.addToolBar('File')
         fileToolBar.addAction(self.exitAction)
 
@@ -124,6 +198,15 @@ class Window(QMainWindow):
         annotToolBar.addAction(self.createAnnotAction)
 
     def createAct(self):
+        """
+            данная функция создает действия и связывает их с методами класса или другими функциями
+            parameters
+
+            self
+            returns
+
+            none
+        """
         self.exitAction = QAction(QIcon('img/exit.png'), '&Exit')
         self.exitAction.triggered.connect(qApp.quit)
 
@@ -143,6 +226,15 @@ class Window(QMainWindow):
         self.createData3Action.triggered.connect(self.createDataset3)
 
     def createAnnotation(self):
+        """
+            данная функция создает аннотацию для текущего датасета
+            parameters
+
+            self
+            returns
+
+            none
+        """
         if 'dataset2' in str(self.folderpath):
             self.annotation2_path = QFileDialog.getExistingDirectory(
                 self, 'Select folder annotation of 2 dataset')
@@ -164,6 +256,15 @@ class Window(QMainWindow):
         else:
             QMessageBox.warning(self, 'Ошибка', 'Выберите папку с исходным датасетом')
     def createDataset2(self):
+        """
+            данная функция создает новый датасет, соединяя имя класса с порядковым номером
+            parameters
+
+            self
+            returns
+
+            none
+        """
         self.dataset2_path = QFileDialog.getExistingDirectory(
             self, 'Select folder for second dataset')
         create_dataset2(self.dataset2_path)
@@ -171,6 +272,15 @@ class Window(QMainWindow):
         self.dataMenu.addAction(self.createData3Action)
 
     def createDataset3(self):
+        """
+            данная функция создает новый датасет с рандомными числами
+            parameters
+
+            self
+            returns
+
+            none
+        """
         self.dataset3_path = QFileDialog.getExistingDirectory(
         self, 'Select folder for third dataset')
         create_dataset3(self.dataset2_path, self.dataset3_path)
@@ -178,6 +288,15 @@ class Window(QMainWindow):
 
 
     def changeDataset(self):
+        """
+            данная функция изменяет текущий датасет
+            parameters
+
+            self
+            returns
+
+            none
+        """
         reply = QMessageBox.question(self, 'Warning', f'Are you sure you want to change current dataset?\nCurrent dataset: {str(self.folderpath)}',
                                      QMessageBox.Yes | QMessageBox.No)
 
@@ -191,6 +310,17 @@ class Window(QMainWindow):
             pass
 
     def closeEvent(self, event: QEvent):
+        """
+            данная функция позволяет спросить пользователя, уверен ли он в том, что хочет закрыть окно
+            parameters
+
+            self
+            event:
+                событие, которое возникает после нажатия на закрытие приложения
+            returns
+
+            none
+        """
         reply = QMessageBox.question(self, 'Warning', 'Are you sure to quit?',
                                      QMessageBox.Yes | QMessageBox.No)
         if reply == QMessageBox.Yes:
@@ -200,6 +330,15 @@ class Window(QMainWindow):
 
 
 def main():
+    """
+        данная функция создает объект приложения
+        parameters
+
+        self
+        returns
+
+        none
+    """
     app = QApplication(sys.argv)
     window = Window()
     sys.exit(app.exec_())
